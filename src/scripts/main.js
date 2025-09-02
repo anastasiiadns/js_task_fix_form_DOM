affecff1d6db20ef96e4b7389e0e4d5404337042
anastasiiadns <nastyadenisenko17@gmail.com>
2025-09-02 17:19:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+for (const form of forms) {
+  const inputs = form.querySelectorAll('input');
+
+  for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+
+    if (!input.id) {
+      input.id = 'input' + i;
+    }
+
+    const label = document.createElement('label');
+
+    label.textContent = 'Text';
+    label.className = 'field-label';
+    label.setAttribute('for', input.id);
+
+    if (input.name) {
+      label.textContent =
+        input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+      input.placeholder = label.textContent;
+    }
+    input.parentNode.appendChild(label);
+  }
+}
